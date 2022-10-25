b7d389f4b71988478ed43620a00bbac4cdab3134
Marvin <bourhanemarvin@hotmail.fr>
2022-10-25 14:02:29 +0200

    my eagle flys

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,48 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff(){
+        if(!this.flying && this.altitude == 0) {
+            this.flying = true;
+            System.out.printf("%s takes off %n", this.getName());
+        }
+    }
+
+    @Override
+    public int ascend(int meters){
+        if(this.flying){
+            this.altitude = Math.max(this.altitude + meters, 325);
+            System.out.printf("%s flys up up and away, at: %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+    @Override
+    public int descend(int meters){
+        if(this.flying){
+            this.altitude = Math.max(this.altitude - meters, 0);
+            System.out.printf("%s flys down, at: %d%n", this.getName(), this.altitude);
+        } 
+        return this.altitude;
+    }
+
+    @Override
+    public void land(){
+        if(this.flying && this.altitude == 0){
+            System.out.println("it landing " + this.getName());
+        }else{
+            System.out.println("lands on the ground " + this.getName());
+        }
+    }
+
+    @Override
+    public void glide(){
+        if(this.flying && this.altitude == 0){
+            System.out.println(" has crashed" + this.getName());
+        } else{
+            System.out.println("glides around " + this.getName());
+        }
     }
 }
